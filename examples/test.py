f18796c2da302af8029c63667cd07dc32492756c
abhibesu2018 <abhibesu@gmail.com>
2018-09-01 20:25:25 +1000

    Updated configurabe userid and password to connect

--- examples/test.py
+++ examples/test.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 #
 # Use text editor to edit the script and type in valid Instagram username/password
-
+from yaml import load
 from InstagramAPI import InstagramAPI
 
-api = InstagramAPI("login", "password")
+with open('/home/abhi/user_instagram.yaml', 'r') as f:
+    doc = load(f)
+api = InstagramAPI(doc["user"]["username"], doc["user"]["password"])
 if (api.login()):
     api.getSelfUserFeed()  # get self user feed
     print(api.LastJson)  # print last response JSON
